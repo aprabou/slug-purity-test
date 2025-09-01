05c4d4169cee69942cd3b36a7d59f1690973c512
aprabou <ashwinprabou16@gmail.com>
2025-09-01 16:52:35 -0700

    supabase db

---
+++ config.js
@@ -0,0 +1,5 @@
+// Supabase configuration
+window.SUPABASE_CONFIG = {
+    url: 'https://fpbybafkpsyfhpepwccf.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwYnliYWZrcHN5ZmhwZXB3Y2NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3Njg2MTUsImV4cCI6MjA3MjM0NDYxNX0.aM6rkRjzBqaeR934GyEhgIzXvXOxnJQHiDvWH64p7jo'
+};
