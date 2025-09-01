724e60709877aa7c7099cebc63f657d305ab6c32
aprabou <ashwinprabou16@gmail.com>
2025-09-01 15:30:29 -0700

    remove

--- run.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-if __name__ == "__main__":
-    # Render uses PORT environment variable
-    import os
-    port = int(os.environ.get('PORT', 5050))
-    app.run(debug=False, host='0.0.0.0', port=port)
