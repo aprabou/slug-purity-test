3063a8e8aecd5895b97a64d522b34f75a6aca547
Ashwin Prabou <119720635+aprabou@users.noreply.github.com>
2025-09-01 16:56:46 -0700

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Supabase configuration
-window.SUPABASE_CONFIG = {
-    url: 'https://fpbybafkpsyfhpepwccf.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwYnliYWZrcHN5ZmhwZXB3Y2NmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3Njg2MTUsImV4cCI6MjA3MjM0NDYxNX0.aM6rkRjzBqaeR934GyEhgIzXvXOxnJQHiDvWH64p7jo'
-};
